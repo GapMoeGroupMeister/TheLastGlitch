793dccd3b204ee437b3e493555a4a1f167db33ee
dlckdgh0414 <ci5976547@gmail.com>
2024-06-05 23:32:55 +0900

    Fix : UpGradeShopName and BoxCast

--- Assets/01_Scripts/LCH/TestPlayer/LchPlayer.cs
+++ Assets/01_Scripts/LCH/TestPlayer/LchPlayer.cs
@@ -6,7 +6,11 @@ public class LchPlayer : MonoBehaviour
 {
     private Rigidbody2D _rigid;
     private float _speed = 5f;
-    private bool waitIsGanghwajang = false;
+    private bool _waitIsUpGradeShop = false;
+    private bool _waitIsShop = false;
+    [SerializeField] private LayerMask _thisIsUpGradeShop;
+    [SerializeField] private LayerMask _thisIsShop;
+    private Vector2 _boxCastSize = new Vector2(1f,1f);
 
     private void Awake()
     {
@@ -19,10 +23,21 @@ public class LchPlayer : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
         Vector2 move = new Vector2(x, y);
         _rigid.velocity = move.normalized * _speed;
+        UpgradeShop();
+        if(_waitIsUpGradeShop)
+        {
+            Debug.Log("°¨ÁöµÊ");
+        }
     }
 
-    private void CheckGanghwajang()
+    private void UpgradeShop()
     {
+        _waitIsUpGradeShop = Physics2D.BoxCast(transform.position,_boxCastSize,0f,transform.position,_thisIsUpGradeShop);
+    }
 
+    private void GizmosDrawBox()
+    {
+        Gizmos.color = Color.red;
+        //Gizmos.DrawCube();
     }
 }
