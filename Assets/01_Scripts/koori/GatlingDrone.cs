fa995982d212672a6af802bc7e5e4f6f5cfabc84
koori0831 <fuyunokoori0831@gmail.com>
2024-07-10 18:49:28 +0900

    feat Dron

---
+++ Assets/01_Scripts/koori/GatlingDrone.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatlingDrone : Drone
+{
+    private void Start()
+    {
+        speed = 2.85f;
+    }
+    protected override void Attack()
+    {
+    }
+}
