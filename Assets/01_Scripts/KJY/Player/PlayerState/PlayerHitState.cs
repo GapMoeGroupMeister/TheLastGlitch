83b3c788ff6229bba36fc359eb51a2f5a8b65488
Cwldb <mozi0810@gmail.com>
2024-07-18 18:31:23 +0900

    feat : PlayerState

---
+++ Assets/01_Scripts/KJY/Player/PlayerState/PlayerHitState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitState : PlayerState<PlayerStateEnum>
+{
+    public PlayerHitState(Player _onwer, StateMachine<PlayerStateEnum> state, string animHashName) : base(_onwer, state, animHashName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+    }
+}
