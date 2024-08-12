7f61bc1611357c72395d3e4e95a6f1eacdae47c1
san232 <ebs00098@gmail.com>
2024-08-12 22:32:32 +0900

    fix : Laser

--- Assets/01_Scripts/San232/Laser.cs
+++ Assets/01_Scripts/San232/Laser.cs
@@ -7,16 +7,19 @@ public class Laser : MonoBehaviour
     [SerializeField] private Transform _target;
     [SerializeField] private GameObject _laserPrefab;
     [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private Transform _muzzlePos;
 
     private void Awake()
     {
+        _muzzlePos.position = gameObject.transform.position;
         _target = GameObject.FindWithTag("Player").transform;
         _lineRenderer = _laserPrefab.GetComponent<LineRenderer>();
+        FireLaser();
     }
 
-    public void FireLaser()
+    private void FireLaser()
     {
-        Vector3 startPosition = transform.position;
+        Vector3 startPosition = _muzzlePos.position;
 
         Vector3 endPosition = _target.position;
 
