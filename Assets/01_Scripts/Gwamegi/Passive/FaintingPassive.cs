d6deb23b6f6ad060a46e3b0724dcb44ae796f020
gominsu08 <minsu080618@gmail.com>
2024-08-20 00:30:38 +0900

    feat : Passive

---
+++ Assets/01_Scripts/Gwamegi/Passive/FaintingPassive.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Player/FaintingSO")]
+public class FaintingPassive : PassiveSO
+{
+    public override void Skill(Agent owner)
+    {
+        
+    }
+}
