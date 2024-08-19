628aae3f91fecf76b0dd1a004b70ca9ff4ff528c
san232 <ebs00098@gmail.com>
2024-08-20 00:50:25 +0900

    remove : LaserSettingSO

--- Assets/01_Scripts/San232/LaserSettingSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO/Laser")]
-public class LaserSettingSO : ScriptableObject
-{
-    public float laserTime;
-}
