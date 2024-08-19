8c9e14d9f0429ce30b32011554dcae254a4c8012
san232 <ebs00098@gmail.com>
2024-08-20 01:45:54 +0900

    feat : MgsyAnimController

--- Assets/01_Scripts/San232/LaserShooter.cs
+++ Assets/01_Scripts/San232/LaserShooter.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class LaserShooter : MonoBehaviour
 {
     [SerializeField] private Laser _laser;
+
+    [Header("Setting")]
     [SerializeField] private float _laserTime = 0.3f;
 
     private void Awake()
