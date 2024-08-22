8f05988a163c57eacefc6e7e5ad1197a33c2a6c8
dlckdgh0414 <ci5976547@gmail.com>
2024-08-22 19:26:38 +0900

    fix : Enemy

--- Assets/01_Scripts/LCH/LchState/Boom/BoomWatingState.cs
+++ Assets/01_Scripts/LCH/LchState/Boom/BoomWatingState.cs
@@ -4,7 +4,24 @@ using UnityEngine;
 
 public class BoomWatingState : EnemyState<EnemyStateEnum>
 {
+    StateManager state;
+    BoomEnemy boom;
     public BoomWatingState(Enemy enemyBase, StateMachine<EnemyStateEnum> stateMachine, string animBoolName) : base(enemyBase, stateMachine, animBoolName)
     {
+        state = enemyBase as StateManager;
+        boom = enemyBase as BoomEnemy;
+    }
+    public override void UpdateState()
+    {
+        if(boom._boomDealy < boom._boomWait + Time.deltaTime&& _enemy.targetTrm.position.x < _enemy.attackRadius)
+        {
+            _stateMachine.ChangeState(EnemyStateEnum.Dead);
+        }
+        else
+        {
+            boom._boomWait = 0f;
+            _stateMachine.ChangeState(EnemyStateEnum.Walk);
+        }
+        base.UpdateState();
     }
 }
