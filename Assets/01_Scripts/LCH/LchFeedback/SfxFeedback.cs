37ac835e264650f77cfb8264f94028a7d7436a5a
dlckdgh0414 <ci5976547@gmail.com>
2024-09-04 18:46:32 +0900

    feat : Ui

---
+++ Assets/01_Scripts/LCH/LchFeedback/SfxFeedback.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxFeedback : Feedback
+{
+    public override void PlayFeedback()
+    {
+       
+    }
+
+    public override void StopFeedback()
+    {
+      
+    }
+}
