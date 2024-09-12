8a5a2608c94cfa0267f9aaf98322711463bd4201
dlckdgh0414 <ci5976547@gmail.com>
2024-09-13 01:45:16 +0900

    fix : Sound

---
+++ Assets/01_Scripts/LCH/SettingManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingManager : MonoBehaviour
+{
+	[SerializeField] private AudioMixer _Audiomixer;
+	[SerializeField] private Slider _musicSlider;
+	[SerializeField] private Slider _sfxSlider;
+}
