47067bbbee05c9f76306e4351684ac52fddbf1d3
dlckdgh0414 <ci5976547@gmail.com>
2024-10-03 00:58:36 +0900

    fix : Stage5

--- Assets/01_Scripts/Gwamegi/GameManager.cs
+++ Assets/01_Scripts/Gwamegi/GameManager.cs
@@ -7,6 +7,11 @@ public class GameManager : MonoSingleton<GameManager>
     private Player player;
     private bool isClear;
 
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     public Player Player
     {
 
