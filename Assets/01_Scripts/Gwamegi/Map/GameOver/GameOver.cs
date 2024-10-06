f74b4ba0852eb58775ab398f7475188e99be94fc
gominsu08 <minsu080618@gmail.com>
2024-10-06 23:33:23 +0900

    feat : none

--- Assets/01_Scripts/Gwamegi/Map/GameOver/GameOver.cs
+++ Assets/01_Scripts/Gwamegi/Map/GameOver/GameOver.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private RectTransform _gameOverPanel;
+    [SerializeField] private TextMeshProUGUI _haveCoinText;
+    [SerializeField] private TextMeshProUGUI _playTimeText;
+
+    public int haveCoin;
+    public float playTime;
 
     private void Update()
     {
         if (GameManager.Instance.Player.IsDie)
         {
             _gameOverPanel.gameObject.SetActive(true);
+            _playTimeText.SetText($"살아남은 시간 : {playTime}");
+            _haveCoinText.SetText($"얻은 코인 : {haveCoin}");
         }
     }
 
