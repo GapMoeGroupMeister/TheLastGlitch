6d8deb2576d4e4efe1b7c7fd64d81829cdb6ae62
koori0831 <fuyunokoori0831@gmail.com>
2024-10-07 21:42:31 +0900

    feat : menuRenual2

--- Assets/01_Scripts/koori/UIOnTheMouse.cs
+++ Assets/01_Scripts/koori/UIOnTheMouse.cs
@@ -1,8 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class UIOnTheMouse : MonoBehaviour
+public class UIOnTheMouse : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    
+    private Vector3 _originalScale;
+    private RectTransform _scale;
+
+    private void Start()
+    {
+        _scale = GetComponent<RectTransform>();
+        _originalScale = _scale.localScale;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _scale.DOScale(_originalScale, 0.5f);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _scale.DOScale(new Vector3(1.3f, 1.3f, 1), 0.5f);
+    }
 }
