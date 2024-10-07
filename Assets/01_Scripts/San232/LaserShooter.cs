ba88da74668e3e074fbd60b585ffd77a4a17a9bb
san232 <ebs00098@gmail.com>
2024-10-07 22:04:48 +0900

    feat : UseShortLaserCheck

--- Assets/01_Scripts/San232/LaserShooter.cs
+++ Assets/01_Scripts/San232/LaserShooter.cs
@@ -2,21 +2,47 @@ using UnityEngine;
 
 public class LaserShooter : MonoBehaviour
 {
-    [SerializeField] private Laser _laser;
+    private Laser _laser;
+    private ShortLaser _shortLaser;
+
+    [Header("Short Laser Check")]
+    [SerializeField] private bool _isUseShortLaser = false;
 
     [Header("Laser Settings")]
     [SerializeField] private float _laserDuration = 0.2f; // 레이저가 타겟에 도달하는 시간
     [SerializeField] private float _laserLifetime = 0.3f; // 레이저가 유지되는 시간
 
+    private void Awake()
+    {
+        if(_isUseShortLaser)
+            _shortLaser = GetComponentInChildren<ShortLaser>(); 
+        else
+            _laser = GetComponentInChildren<Laser>();
+    }
+
     public void FireLaser(Transform firePos, Transform target)
     {
-        _laser.gameObject.SetActive(true);
+        if( _isUseShortLaser)
+        {
+            _shortLaser.gameObject.SetActive(true);
 
-        // 목표 방향을 계산합니다.
-        Vector3 direction = (target.position - firePos.position).normalized;
+            // 목표 방향을 계산합니다.
+            Vector3 direction = (target.position - firePos.position).normalized;
 
-        // 레이저 위치 설정
-        _laser.SetLaserPositions(firePos.position, direction);
-        _laser.ActivateLaser(firePos.position, direction, _laserDuration, _laserLifetime); // 레이저 발사
+            // 레이저 위치 설정
+            _shortLaser.SetLaserPositions(firePos.position, direction);
+            _shortLaser.ActivateLaser(firePos.position, direction, _laserDuration, _laserLifetime); // 레이저 발사
+        }
+        else
+        {
+            _laser.gameObject.SetActive(true);
+
+            // 목표 방향을 계산합니다.
+            Vector3 direction = (target.position - firePos.position).normalized;
+
+            // 레이저 위치 설정
+            _laser.SetLaserPositions(firePos.position, direction);
+            _laser.ActivateLaser(firePos.position, direction, _laserDuration, _laserLifetime); // 레이저 발사
+        }
     }
 }
