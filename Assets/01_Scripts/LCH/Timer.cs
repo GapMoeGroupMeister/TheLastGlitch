0840d9a6179a5973fddead31c03ad2366e4b9aea
dlckdgh0414 <ci5976547@gmail.com>
2024-10-07 20:04:11 +0900

    feat : Timer

--- Assets/01_Scripts/LCH/Timer.cs
+++ Assets/01_Scripts/LCH/Timer.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-	
+    [SerializeField] private TextMeshProUGUI _textTmp;
+    private float _timer = Time.time;
+
+    public float TimeerSet
+    {
+        get => _timer;
+        set => _timer += value;
+    }
+
+    private void Update()
+    {
+        _textTmp.text = $"Timer : {Mathf.FloorToInt(_timer)}";
+    }
 }
