a530207a4596ed39be53dcc4b15d7633d1620aaf
gominsu08 <minsu080618@gmail.com>
2024-10-09 20:34:18 +0900

    fix : EnemyHPBarFlip

--- Assets/01_Scripts/Gwamegi/Map/Stage.cs
+++ Assets/01_Scripts/Gwamegi/Map/Stage.cs
@@ -1,16 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Stage : MonoBehaviour
 {
+    private void Awake()
+    {
+        Player2ActiveSkill.Instance.ErrorActive();
+
+    }
+
     void Start()
     {
         GameManager.Instance.Player.gameObject.SetActive(true);
         GameManager.Instance.Player.GetComponent<PlayerDash>().DashBool();
         GameManager.Instance.Player.MovementComponent._canMove = true;
         WeaponCoolTime.instance._attack = false;
-        Player2ActiveSkill.Instance.ErrorActive();
 
     }
 }
