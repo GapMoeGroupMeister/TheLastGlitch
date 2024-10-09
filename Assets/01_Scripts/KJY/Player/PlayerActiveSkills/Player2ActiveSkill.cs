d0650a2551aa6db508bc4ab74f8c22386369a0b0
Cwldb <mozi0810@gmail.com>
2024-10-09 14:42:10 +0900

    fix : ActiveReturn

--- Assets/01_Scripts/KJY/Player/PlayerActiveSkills/Player2ActiveSkill.cs
+++ Assets/01_Scripts/KJY/Player/PlayerActiveSkills/Player2ActiveSkill.cs
@@ -16,6 +16,7 @@ public class Player2ActiveSkill : MonoBehaviour
 
     private Health _playerHealth;
     private PlayerStat _playerStat;
+    private AgentMovement _agent;
 
     Stat _upStat = new Stat();
     Stat _downStat = new Stat();
@@ -45,8 +46,6 @@ public class Player2ActiveSkill : MonoBehaviour
     private void UseActive()
     {
         GetDamage();
-
-        StartCoroutine(returnStat());
     }
 
     private void GetDamage()
@@ -57,15 +56,36 @@ public class Player2ActiveSkill : MonoBehaviour
             _activeHp = _currentHealth;
             _playerHealth.TakeDamage(_currentHealth, Vector2.zero, 0);
             PowerUp();
+
+            StartCoroutine(returnStat());
         }
     }
 
     private void PowerUp()
     {
+        Debug.Log("UpStat");
         _upStat.atkPower += _atkStat;
         _upStat.moveSpeed += _speedStat;
         _upStat.critDamage += _critDmgStat;
         _playerStat.StatSet(_upStat);
+    }
+
+    private void ReturnPower()
+    {
+        Debug.Log("DownStat");
+
+        Stat downStat = new Stat();
+        downStat.atkPower += -_atkStat;
+        downStat.moveSpeed += -_speedStat;
+        downStat.critDamage += -_critDmgStat;
+
+        _playerStat._moveSpeed = 0;
+        _playerStat._critDamage = 0;
+        _playerStat._atkPower = 0;
+
+        _playerStat.StatSet(downStat);
+        _activeCool = false;
+
     }
 
     private IEnumerator returnStat()
@@ -73,9 +93,6 @@ public class Player2ActiveSkill : MonoBehaviour
         _activeCool = true;
         yield return new WaitForSeconds(5f);
         _playerHealth.CurrentHealth += ((int)_activeHp);
-        _downStat.atkPower -= _atkStat;
-        _downStat.moveSpeed -= _speedStat;
-        _downStat.critDamage -= _critDmgStat;
-        _playerStat.StatSet(_downStat);
+        ReturnPower();
     }
 }
