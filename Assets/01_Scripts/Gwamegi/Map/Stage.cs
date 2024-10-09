db2dc9f16f6fc45ec0cb8d882a3ee584349b4239
gominsu08 <minsu080618@gmail.com>
2024-10-09 17:22:24 +0900

    fix : PlayerDash

--- Assets/01_Scripts/Gwamegi/Map/Stage.cs
+++ Assets/01_Scripts/Gwamegi/Map/Stage.cs
@@ -7,6 +7,7 @@ public class Stage : MonoBehaviour
     void Start()
     {
         GameManager.Instance.Player.gameObject.SetActive(true);
+        GameManager.Instance.Player.GetComponent<PlayerDash>().DashBool();
         GameManager.Instance.Player.MovementComponent._canMove = true;
         WeaponCoolTime.instance._attack = false;
         Player2ActiveSkill.Instance.ErrorActive();
